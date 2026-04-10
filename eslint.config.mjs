6078281009e0d8bdd7d434f9e9b0c82da3df7a92
Uvarov1tte <ngqchia@gmail.com>
2026-04-10 21:38:42 +0700

    make eslint no longer trigger no-undef errors on global variables

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,6 +6,7 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
+import globals from "globals";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -26,6 +27,7 @@ export default defineConfig([{
     languageOptions: {
         globals: {
             ...reactNative.environments["react-native"]["react-native"],
+            ...globals.node,
         },
         ecmaVersion: "latest",
         sourceType: "module",
